bec116e73ee960bcaaea546323eb2b0613c676c1
user1 <shadow1212@21cn.com>
2018-09-10 09:58:00 +0800

    minor changes on the rb tree find/insert

--- myrbtree.h
+++ myrbtree.h
@@ -3,43 +3,43 @@
 #define __MYRBTREE_H__
 
 
-#define MY_RB_TREE_INSERT(root, item, k, node) ({  \
-  struct rb_node **p = &(root)->rb_node, *parent = 0 ;  \
-  typeof(item) prt ;  \
-  while (*p) {  \
-    parent  = *p ;  \
-    prt = rb_entry(parent,typeof((*(item))),node);  \
-    if (prt->k < (item)->k) {  \
-      p = &parent->rb_left ;  \
+#define MY_RB_TREE_INSERT(__root__, __i__, __k__, __nd__) ({  \
+  struct rb_node **__p__ = &(__root__)->rb_node, *__parent__ = 0 ;  \
+  typeof(__i__) __prt__ ;  \
+  while (*__p__) {  \
+    __parent__  = *__p__ ;  \
+    __prt__ = rb_entry(__parent__,typeof((*(__i__))),__nd__);  \
+    if (__prt__->__k__ < (__i__)->__k__) {  \
+      __p__ = &__parent__->rb_left ;  \
     } else {   \
-      p = &parent->rb_right ;  \
+      __p__ = &__parent__->rb_right ;  \
     }  \
   }   \
-  /* insert new node */   \
-  (item)->node.__rb_parent_color = (unsigned long)parent; /* FIXME: why ?? */  \
-  (item)->node.rb_left = (item)->node.rb_right = NULL ;   \
-  *p = &(item)->node ;   \
+  /* insert new __nd__ */   \
+  (__i__)->__nd__.__rb_parent_color = (unsigned long)__parent__; /* FIXME: why ?? */  \
+  (__i__)->__nd__.rb_left = (__i__)->__nd__.rb_right = NULL ;   \
+  *__p__ = &(__i__)->__nd__ ;   \
   /* change color */    \
-  rb_insert_color(&(item)->node,root);  \
+  rb_insert_color(&(__i__)->__nd__,__root__);  \
   0;  \
 })
 
 
-#define MY_RB_TREE_FIND(root, key, res, k, node) ({  \
-  struct rb_node **p = &(root)->rb_node, *parent = 0 ;  \
-  typeof(res) prt ;   \
-  int rc = 0;   \
-  while (*p) {   \
-    parent  = *p ;   \
-    prt = rb_entry(parent,typeof((*(res))),node);  \
-    if (prt->k < key)  p = &parent->rb_left ;    \
-    else if (prt->k > key)  p = &parent->rb_right ;   \
+#define MY_RB_TREE_FIND(__root__, __key__, __res__, __k__, __nd__) ({  \
+  struct rb_node **__p__ = &(__root__)->rb_node, *__parent__ = 0 ;  \
+  typeof(__res__) __prt__ ;   \
+  int __rc__ = 0;   \
+  while (*__p__) {   \
+    __parent__  = *__p__ ;   \
+    __prt__ = rb_entry(__parent__,typeof((*(__res__))),__nd__);  \
+    if (__prt__->__k__ < __key__)  __p__ = &__parent__->rb_left ;    \
+    else if (__prt__->__k__ > __key__)  __p__ = &__parent__->rb_right ;   \
     else  break ;   \
   }   \
   /* not found */  \
-  if (!(*p)) rc = -1;   \
-  else  res = prt ; \
-  rc;   \
+  if (!(*__p__)) __rc__ = -1;   \
+  else  __res__ = __prt__ ; \
+  __rc__;   \
 })
 
 #endif /* __MYRBTREE_H__*/
